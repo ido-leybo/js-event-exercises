0312629d69cbc7da96411062373bd3f94a28d9ee
Ido Leybovitch <77382724+ido-leybo@users.noreply.github.com>
2021-01-25 17:54:00 -0800

    fixed the javascript file
    fixed the javascript file

--- number 2-2 Tasks/catch links in the element/main.js
+++ number 2-2 Tasks/catch links in the element/main.js
@@ -1,12 +1,17 @@
 const links = document.querySelectorAll('a');
+console.log(links);
 
 for (let link of links) {
     link.addEventListener("click", event => {
         let yesOrNo = confirm("You want to move to: " + link);
-        if (!yesOrNo) {
-            link.setAttribute("href", '#');
-        } 
-    });
+        console.log(yesOrNo)
+        if (yesOrNo) {
+            return true;
+        } else {
+            event.stopPropagation();
+            event.preventDefault();
+        }
+    });    
 };
 
 
