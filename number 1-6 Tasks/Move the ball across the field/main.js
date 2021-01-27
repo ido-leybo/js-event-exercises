38576ffcb8355c6c5e1f16f33715feaf4719ca35
Ido Leybovitch <77382724+ido-leybo@users.noreply.github.com>
2021-01-26 23:06:12 -0800

    number 1- move the ball
    fixed main.js file

--- number 1-6 Tasks/Move the ball across the field/main.js
+++ number 1-6 Tasks/Move the ball across the field/main.js
@@ -4,22 +4,27 @@ const field = document.getElementById('field');
 
 field.addEventListener("click", event => {
     let fieldPosition = field.getBoundingClientRect();
-    console.log(fieldPosition)
+    
     let ballPosition = {
-        top: event.clientY - fieldPosition.top - field.clientTop - ball.clientHeight / 2,
-        left: event.clientX - fieldPosition.left - field.clientLeft - ball.clientWidth / 2
+        top: (event.clientY - fieldPosition.top - field.clientTop - ball.clientHeight / 2),
+        left: (event.clientX - fieldPosition.left - field.clientLeft - ball.clientWidth / 2)
     }
-    console.log(ball.clientHeight)
-    console.log(field.clientLeft)
-    console.log(event.clientX)
+    
+    console.log(field.clientWidth);
+    console.log(fieldPosition);
     if (ballPosition.left < 0) {
         ballPosition.left = 0;
     } else if (ballPosition.top < 0) {
         ballPosition.top = 0;
+    } else if (ballPosition.left + ball.clientWidth > field.clientWidth) {
+        ballPosition.left = field.clientWidth - ball.clientWidth;
+    } else if (ballPosition.top + ball.clientHeight > field.clientHeight) {
+        ballPosition.top = field.clientHeight - ball.clientHeight;;
     }
+    
     ball.style.left = ballPosition.left + 'px';
     ball.style.top = ballPosition.top + 'px';
-    console.log(ball.style.left)
+    
 });
 
 
