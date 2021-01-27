6400e1e6e04eb86ecf8228f31e77f8932484bb8b
Ido Leybovitch <77382724+ido-leybo@users.noreply.github.com>
2021-01-26 20:30:41 -0800

    number 3 - fixed tooltip
    fixed html, css and javascript files

--- number 3 - 3 Tasks/Tooltip behavior/main.js
+++ number 3 - 3 Tasks/Tooltip behavior/main.js
@@ -1,19 +1,20 @@
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
+const buttons = document.querySelectorAll('button');
+const tooltip1 = document.getElementById('tool');
+const tooltip2 = document.getElementById('tool2');
 
-button1.addEventListener("mouseover", () => {
-    button1.setAttribute("data-toggle", "tooltip");
-    button1.setAttribute("title", "the tooltip is longer than the element");
-});
-button2.addEventListener("mouseover", () => {
-    button2.setAttribute("data-toggle", "tooltip");
-    button2.setAttribute("title", "HTML\ntooltip");
-});
-button1.addEventListener("mouseout", () => {
-    button1.removeAttribute("data-toggle");
-    button1.removeAttribute("title");
-});
-button2.addEventListener("mouseout", () => {
-    button2.removeAttribute("data-toggle");
-    button2.removeAttribute("title");
-});
+tooltip1.hidden = true;
+tooltip2.hidden = true;
+
+for (let button of buttons) {
+    button.addEventListener("mouseover", event => {
+        if (button.className === 'tool') {
+            tooltip1.hidden = false;
+        } else if (button.className === 'tool2') {
+            tooltip2.hidden = false;
+        };
+        button.addEventListener("mouseout", event => {
+        tooltip1.hidden = true;
+        tooltip2.hidden = true;
+        });
+    });
+};
